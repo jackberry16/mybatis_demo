2a412e44c7b1f6423f1f5bb678380912241d0664
chengh <821635867@qq.com>
2022-08-08 23:21:01 +0800

    增加

---
+++ src/main/java/com/example/mybatis_demo/entity/Score.java
@@ -0,0 +1,41 @@
+package com.example.mybatis_demo.entity;
+
+public class Score {
+    private String type;
+    private int sc;
+    private String name;
+    private String address;
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getSc() {
+        return sc;
+    }
+
+    public void setSc(int sc) {
+        this.sc = sc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
